6749d7c749bedf6c5136e1576a4b4a566dfd9a17
Bhuvan Parashar <parasharbhuvan04@gmail.com>
2024-02-10 17:08:23 +0530

    initial commit

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+    }
+}
